60e2363e684859be96161c1a8e25f2cdbc9826dc
deepaksingh1227 <divyanthakur856@gmail.com>
2026-06-12 11:28:33 +0530

    UseCase of throws keyword

---
+++ ExceptionHandling/ThrowsDemo.java
@@ -0,0 +1,30 @@
+package ExceptionHandling;
+
+public class ThrowsDemo {
+    public class Library {
+        int availableBooks = 3;
+
+        public void borrowBook(int bookRequested) throws Exception {
+            if (bookRequested > availableBooks) {
+                throw new Exception("Not enough books available");
+            }
+
+        }
+
+    }
+
+    public static void main(String args[]) {
+        ThrowsDemo obj = new ThrowsDemo();
+
+        Library l1 = obj.new Library();
+        try {
+            l1.borrowBook(5);
+        } catch (Exception e) {
+            // e.printStackTrace();
+            // System.out.println("Exception Occured");
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+}
