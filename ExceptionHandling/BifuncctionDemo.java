d05661f5a06347940aac257c1de1398434de4bc5
deepaksingh1227 <divyanthakur856@gmail.com>
2026-06-13 11:18:18 +0530

    USe of method refrence in functional interface or in llambda expressions

---
+++ ExceptionHandling/BifuncctionDemo.java
@@ -0,0 +1,15 @@
+package ExceptionHandling;
+
+import java.util.function.BiFunction;
+
+public class BifuncctionDemo {
+    public static BiFunction<Integer, Integer, Integer> addFunction = Integer::sum;
+
+    public static BiFunction<Integer, Integer, Integer> substractFunction = (a, b) -> a - b;
+
+    public static void main(String args[]) {
+        System.out.println(addFunction.apply(10, 20));
+        System.out.println(substractFunction.apply(45, 16));
+
+    }
+}
