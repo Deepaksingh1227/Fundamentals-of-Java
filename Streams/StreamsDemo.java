0f22575c272b7401837e53665422986cd2fcc4ed
deepaksingh1227 <divyanthakur856@gmail.com>
2026-06-14 22:00:47 +0530

    Stream Basics

---
+++ Streams/StreamsDemo.java
@@ -0,0 +1,18 @@
+package Streams;
+import java.util.*;
+
+
+public class StreamsDemo {
+    // Streams --> Assembly line / pipeline
+    public static void main (String args[]){
+        List <String> list= Arrays.asList("Apple", "Mango", "Banana");
+        for(String fruits: list){
+            System.out.println(fruits);
+        }
+
+    }
+
+
+        
+
+}
