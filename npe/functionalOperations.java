bc49e07b99f27b95895700041e45e81bbede0a38
deepaksingh1227 <divyanthakur856@gmail.com>
2026-06-23 10:58:31 +0530

    functionalOperational in java

---
+++ npe/functionalOperations.java
@@ -0,0 +1,34 @@
+package npe;
+
+import java.util.*;
+
+public class functionalOperations {
+    public static void main(String args[]) {
+
+        Optional<String> name = Optional.of("java");
+        Optional<String> nullOptional = Optional.ofNullable(null);
+        // ifPresent()
+
+        name.ifPresent(value -> {
+            System.out.println(value);
+            System.out.println("Hy");
+
+        });
+        nullOptional.ifPresent(value -> {
+            System.out.println(value);
+            System.out.println("Hy");
+
+        });
+
+        // map()
+
+        Optional<String> upperCase = name.map(value -> value.toUpperCase());
+        System.out.println(upperCase);
+
+        // filter()
+        name.filter(n -> n.startsWith("J"))
+                .ifPresent(System.out::println);
+
+    }
+
+}
